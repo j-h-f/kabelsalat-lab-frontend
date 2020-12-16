d0f736e0448ad15556ea879eb3218b7e6022db35
Jan-Hendrik <jan-hendrik.fabricius@t-online.de>
2020-12-16 12:21:22 +0100

    created first prototype

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Über das Projekt</h1>
+    <article>
+      <div>
+        <h3>Hydroponisches Gewächshaus</h3>
+        <div>
+          <p>
+            <span>
+              In dem Projekt geht es darum, ein vollautomatisches,
+              hydroponisches Gewächshaus zu entwickeln, in dem dann Salat und
+              Erdbeeren gezüchtet werden können.
+            </span>
+          </p>
+        </div>
+      </div>
+      <div>
+        <figure>
+          <img
+            src="https://www.trees.com/sites/default/files/inline-images/vertical-hydroponics.png"
+            alt="Hydroponisches Gewächshaus"
+          />
+          <figcaption>
+            Bildquelle:
+            https://www.trees.com/gardening-and-landscaping/vertical-hydroponics
+          </figcaption>
+        </figure>
+      </div>
+    </article>
   </div>
 </template>
+
+<style lang="scss" scoped>
+article {
+  width: 80%;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #8ec647;
+  font-size: 1.5em;
+  color: #434343;
+  display: flex;
+}
+</style>
